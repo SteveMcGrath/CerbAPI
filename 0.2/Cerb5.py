e972ccd64dd3feb12c5afe9cd499dee2db751e47
SteveMcGrath <steve@chigeek.com>
2010-06-14 18:02:53 +0000

    Cleaned up Cerb5 lib

--- 0.2/Cerb5.py
+++ 0.2/Cerb5.py
@@ -5,8 +5,7 @@ import datetime
 import hashlib
 import base64
 import sys
-from xml.etree  import ElementTree as etree
-from xml.dom    import minidom
+import json
 
 DEBUG = False
 
@@ -34,142 +33,11 @@ class API(object):
   def delete(self, url):
     return self.__connect('DELETE', url)
   
-  def listTickets(self):
-    return self.__getList('tickets/list')
-  
-  def listOrgs(self):
-    return self.__getList('orgs/list')
-  
-  def listAddresses(self):
-    return self.__getList('addresses/list')
-  
-  def listMessages(self, ticket):
-    return self.__getList('messages/list?ticket_id=%s' % ticket)
-  
-  def listComments(self, ticket):
-    return self.__getList('comments/list?ticket_id=%s' % ticket)
-  
-  def listTasks(self):
-    return self.__getList('tasks/list')
-  
-  def getTicket(self, ticket):
-    return self.__genDict(
-            etree.fromstring(
-              self.get('tickets/%s' % str(ticket))))
-  
-  def getComment(self, comment):
-    return self.__genDict(
-            etree.fromstring(
-              self.get('comments/%s' % str(comment))))
-  
-  def getNote(self, note):
-    return self.__genDict(
-            etree.fromstring(
-              self.get('notes/%s' % str(note))))
-  
-  def createNote(self, **args):
-    return self.__genDict(
-            etree.fromstring(
-              self.post('notes/create', 
-                self.__genPayload('note', **args))))
-  
-  def createAddresses(self, **args):
-    return self.__genDict(
-            etree.fromstring(
-              self.post('addresses/create', 
-                self.__genPayload('address', **args))))
-  
-  def createComment(self, **args):
-    return self.__genDict(
-            etree.fromstring(
-              self.post('comments/create', 
-                self.__genPayload('comment', **args))))
-  
-  def createOrg(self, **args):
-    return self.__genDict(
-            etree.fromstring(
-              self.post('orgs/create', 
-                self.__genPayload('org', **args))))
-  
-  def updateTicket(self, ticket, **args):
-    return self.__genDict(
-            etree.fromstring(
-              self.post('tickets/%s' % ticket, 
-                self.__genPayload('ticket', **args))))
-  
-  def updateAddress(self, address, **args):
-    return self.__genDict(
-            etree.fromstring(
-              self.post('addresses/%s' % address, 
-                self.__genPayload('address', **args))))
-  
-  def updateOrg(self, org, **args):
-    return self.__genDict(
-            etree.fromstring(
-              self.post('orgs/%s' % org, 
-                self.__genPayload('org', **args))))
-
-  def deleteComment(self, comment):
-    return self.delete('comments/%s' % str(comment))
-  
-  def deleteNote(self, note):
-    return self.delete('notes/%s' % str(note))
-  
-  def find(self, dataType, **args):
-    params = []
-    for param in args:
-      params.append('<%s oper="%s" value="%s"/>' % (param, 
-                                                    params[param]['oper'],
-                                                    params[param]['value']))
-    payload = '''
-    <search>
-      <params>
-        %s
-      </params>
-    </search>
-    ''' % '\n    '.join(params)
-    return self.__genDict(
-            etree.fromstring(
-              self.post('%s/search' % dataType, payload)))
-  
-  
-  def listArticles(self, sort=None, limit=None, root=None):
-    search = []
-    searchCriteria = ''
-    if sort is not None:
-      search.append('sort=%s' & str(sort))
-    if limit is not None:
-      search.append('limit=%s' & str(limit))
-    if root is not None:
-      search.append('root=%s' & str(root))
-    if len(search) > 0:
-      searchCriteria = '?' + '&'.join(search)
-    return self.__getList('articles/list%s' % searchCriteria)
-  
-  def __genPayload(self, tag, **elements):
-    lines = []
-    for element in elements:
-      lines.append('<%s>%s</%s>' % (element, elements[element], element))
-    return '<%s>\n  %s\n</%s>' % (tag, '\n  '.join(lines), tag)
-  
   def __getRFC822Date(self):
     return rfc822.formatdate(
             rfc822.mktime_tz(
               rfc822.parsedate_tz(
                 datetime.datetime.now().strftime('%a, %d %b %Y %H:%M:%S'))))
-  
-  def __genDict(self, tree):
-    ret_dict = {}
-    for item in tree.getchildren():
-      ret_dict[item.tag] = item.text
-    return ret_dict
-  
-  def __getList(self, url):
-    tree = etree.fromstring(self.get(url))
-    itemList = []
-    for item in tree.getchildren():
-      itemList.append(self.__genDict(item))
-    return itemList
   
   def __connect(self, verb, url, payload={}):
     headers   = {
@@ -229,52 +97,4 @@ class API(object):
         print '%20s : %s' % (header, response[header])
       print 'PAYLOAD:'
       print data
-    return data
-
-
-def runTests(api):
-  
-  # Setting Debug for the tests
-  DEBUG = True
-  
-  # Basic Get Test
-  tickets       = api.listTickets()
-  for ticket in tickets:
-    print '     Ticket ID : %s' % ticket['id']
-    print 'Ticket Subject : %s' % ticket['subject']
-    print 'Message Count  : %s\n' % len(api.listMessages(ticket['id']))
-  
-  # Getting Messages for a ticket.
-  messages = api.listMessages(99)
-  for message in messages:
-    print '--- Message ---'
-    for item in message:
-      print '%20s : %s' % (item, message[item])
-  
-  # Testing creation functionality
-  response = api.createNote(message_id=99, 
-                            worker_id=622, 
-                            content='Test Comment')
-  
-  note = api.getNote(1)
-  print '--- Note ---'
-  for item in note:
-    print '%20s : %s' % (item, note[item])
-  
-  print api.deleteNote(1)
-  
-def basic_auth_test():
-  api = API('steve@localhost','linuxx','http://192.168.101.142/helpdesk/index.php/rest')
-  print api.get('tickets/list')
-
-
-if __name__ == '__main__':
-  if len(sys.argv) >= 4:
-    access_key    = sys.argv[1]
-    secret_key    = sys.argv[2]
-    base          = sys.argv[3]
-    api           = API(access_key, secret_key, base)
-    runTests(api)
-  else:
-    print 'Exmaple usage is as follows:'
-    print '%s ACCESS_KEY SECRET_KEY BASE_URL' % sys.argv[0]
+    return json.loads(data)
