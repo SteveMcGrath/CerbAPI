8e0f5d15f7e7992bf35a9d96ce07af63a1372ab0
Steven McGrath <steve@chigeek.com>
2010-11-30 19:33:23 -0600

    Removing Dev code from master tree

--- dev/__init__.py
+++
@@ -1,2 +0,0 @@
-from Cerb4 import API as Cerb4
-from Cerb5 import API as Cerb5
--- dev/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-setup(name='cerbapi',
-      version='0.2',
-      package_dir={'pycerbapi': ''},
-      packages=['pycerbapi'],
-      )
